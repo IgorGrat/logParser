37e13b8ac4a14400f2d974a07c798c445dec6813
uncle <igorgrat@gmail.com>
2025-02-04 11:54:14 +0200

    Update EDG 04.02.2025

--- src/main/java/ua/edg/conector/UpdaterFormalVacations.java
+++ src/main/java/ua/edg/conector/UpdaterFormalVacations.java
@@ -5,12 +5,9 @@ import ua.edg.managerVacation.MainRequest;
 
 import java.util.List;
 
-import static ua.edg.managerVacation.MainRequest.getUsersVacation;
-
 public class UpdaterFormalVacations{
   public static void updateVacation(){
     List<SchedulePeriodUser> scope = MainRequest.getUsersVacation();
     MainRequest.saveDirectUserVacation(scope);
-    /* После этого меняем readObject на boolean из потока */
   }
 }
--- src/main/java/ua/edg/logparser/gui/Main.java
+++ src/main/java/ua/edg/logparser/gui/Main.java
@@ -5,12 +5,10 @@
  import ua.edg.conector.UpdaterFormalVacations;
 
  public class Main {
-   public static void main(String[] args) {
+   public static void main(String[] args){
      int port = Integer.parseInt(args[0]);
      String path = args[1];
      (new Thread(() -> (new ServerTranslator()).turnOnListener(port))).start();
      SwingUtilities.invokeLater(() -> new Panel(port, path));
-     /* Первый этап ***********************************************/
-//     UpdaterFormalVacations.updateVacation();
    }
  }
